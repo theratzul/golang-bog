9512b46f19625a1c8510c1f7f317964de514cd73
Bogdan <bogdan@localhost.localdomain>
2024-01-17 15:21:39 +0200

    feature/basic-golang

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 # sudo go install -v golang.org/x/tools/cmd/goimports@latest
 # go run print-fmt.go
+# go run print-golang.go
---
+++ print-golang.go
@@ -0,0 +1,9 @@
+package main
+// fmt is the library for formatting
+import "fmt"
+
+//  main is the main function
+func main() {
+	// the methods start with capital Print
+	fmt.Println("Hell world")
+}
